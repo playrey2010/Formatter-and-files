545f54b0de128494e11f598dd93a4a19dd707b6d
playrey2010 <playrey2010@gmail.com>
2020-02-07 10:44:29 -0500

    inserted content to new file

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@ import java.util.Formatter;
 public class Main {
     public static void main(String[] args) {
         try {
-            Formatter formatter = new Formatter("C:\\Users\\playr\\Documents\\newfile.txt");
+            Formatter formatter = new Formatter("C:\\Users\\[username]\\Documents\\newfile.txt"); // Change username field
+            formatter.format("%s %s", "Hello", "World! ");
+            formatter.format("%s", "My name is Slim Shady. ");
+            formatter.close();
         } catch (Exception e) {
-            System.out.println("There was an error while creating the file. ");
+            System.out.println("There was an error while creating the file."); // IMPORTANT: when the error happens, the file is overwritten as blank!
         }
     }
 }
